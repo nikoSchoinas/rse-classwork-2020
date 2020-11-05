d601ea40cd017d2e19cb2103a5811937eae21a4f
Nikolaos Schoinas <dai16167@uom.edu.gr>
2020-11-05 13:31:04 +0000

    Add four tests to check the code. Fix the bug. Answers UCL-RITS/rse-classwork-2020#86

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,8 +1,29 @@
-from times import *
+from times import time_range, compute_overlap_time
 
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     result = compute_overlap_time(large,short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+def test_ranges_not_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00")
+    short = time_range("2010-01-12 11:30:00", "2010-01-12 12:00:00", 2, 60)
+    result = compute_overlap_time(large,short)
+    expected = []
+    assert result == expected
+
+def test_ranges_with_intervals():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00",8)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 11:00:00", 2)
+    result = compute_overlap_time(large,short)
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:45:00'),('2010-01-12 10:45:00', '2010-01-12 11:00:00')]
+    assert result == expected
+
+def test_ranges_start_end():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 11:00:00")
+    result = compute_overlap_time(large,short)
+    expected = []
     assert result == expected
