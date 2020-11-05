e813ba0f3136015b89018a8f78afd7fabc66d4ac
Nikolaos Schoinas <dai16167@uom.edu.gr>
2020-11-05 13:48:49 +0000

    Add parametrised testing. Answers UCL-RITS/rse-classwork-2020#89

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -1,7 +1,7 @@
 from times import time_range, compute_overlap_time
 import pytest
 
-def test_given_input():
+""" def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     result = compute_overlap_time(large,short)
@@ -27,8 +27,16 @@ def test_ranges_start_end():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 11:00:00")
     result = compute_overlap_time(large,short)
     expected = []
-    assert result == expected
+    assert result == expected """
 
 def test_time_goes_backwards(): # this is the negative test
     with pytest.raises(ValueError):
         time_range("2010-01-12 10:15:00","2010-01-12 10:00:00")
+
+@pytest.mark.parametrize("test_input,expected",[('compute_overlap_time(time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00"),time_range("2010-01-12 11:30:00", "2010-01-12 12:00:00"))',[])
+                                               ,('compute_overlap_time(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00",8),time_range("2010-01-12 10:30:00", "2010-01-12 11:00:00",2))',[('2010-01-12 10:30:00', '2010-01-12 10:45:00'), ('2010-01-12 10:45:00', '2010-01-12 11:00:00')])
+                                               ,('compute_overlap_time(time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00"),time_range("2010-01-12 10:30:00", "2010-01-12 11:00:00"))',[])
+                                               ,('compute_overlap_time(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60))',[('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')])
+                                               ]) # the last row is a valid one.
+def test_eval(test_input,expected):
+    assert eval(test_input) == expected
