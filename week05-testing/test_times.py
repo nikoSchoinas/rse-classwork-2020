e22dce2593b9a9f2823d03fda7cd97c862c01d87
Nikolaos Schoinas <dai16167@uom.edu.gr>
2020-11-10 19:53:28 +0000

    Add comments

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -33,6 +33,7 @@ def test_time_goes_backwards(): # this is the negative test
     with pytest.raises(ValueError):
         time_range("2010-01-12 10:15:00","2010-01-12 10:00:00")
 
+#a parametrised test. This saves time and code
 @pytest.mark.parametrize("test_input,expected",[('compute_overlap_time(time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00"),time_range("2010-01-12 11:30:00", "2010-01-12 12:00:00"))',[])
                                                ,('compute_overlap_time(time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00",8),time_range("2010-01-12 10:30:00", "2010-01-12 11:00:00",2))',[('2010-01-12 10:30:00', '2010-01-12 10:45:00'), ('2010-01-12 10:45:00', '2010-01-12 11:00:00')])
                                                ,('compute_overlap_time(time_range("2010-01-12 10:00:00", "2010-01-12 10:30:00"),time_range("2010-01-12 10:30:00", "2010-01-12 11:00:00"))',[])
